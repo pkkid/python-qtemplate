e75dc76581fce52fed772caa7a6c2990069e68ba
Michael Shepanski <michael.shepanski@gmail.com>
2023-03-16 00:02:54 -0400

    Add DraggableMixin next to Draggable(Widget)

--- qtemplate/helpers/draggable.py
+++ qtemplate/helpers/draggable.py
@@ -3,15 +3,13 @@ from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
 
 
-class Draggable(QtWidgets.QWidget):
-    """ Allows dragging the parent window. """
+class DraggableMixin:
+    """ Draggable Mixin, allows specifying a parent window to be moved. """
 
-    def __init__(self, window, *args, **kwargs):
-        super(Draggable, self).__init__(*args, **kwargs)
-        self.setProperty('class', 'titlebar')
+    def __init__(self, window=None):
+        self.window = self if window is None else window
         self._dragMousePos = None
         self._dragWidgetPos = None
-        self.window = window
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -22,7 +20,19 @@ class Draggable(QtWidgets.QWidget):
         if self._dragMousePos:
             delta = event.globalPos() - self._dragMousePos
             self.window.move(self._dragWidgetPos + delta)
-
+                
     def mouseReleaseEvent(self, event):
+        if self._dragMousePos:
+            delta = event.globalPos() - self._dragMousePos
+            if (delta.x() != 0 or delta.y() != 0) and hasattr(self, 'widgetMoved'):
+                self.widgetMoved(self.pos())
         self._dragMousePos = None
         self._dragWidgetPos = None
+
+
+class Draggable(DraggableMixin, QtWidgets.QWidget):
+    """ Draggable QWidget, allows specifying a parent window to be moved. """
+
+    def __init__(self, window=None, *args, **kwargs):
+        QtWidgets.QWidget.__init__(self, *args, **kwargs)
+        DraggableMixin.__init__(self, window)
