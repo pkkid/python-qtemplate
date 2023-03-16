93b329627ddb0b04255d8e5f71c50402d3057dff
Michael Shepanski <michael.shepanski@gmail.com>
2023-03-16 16:48:20 -0400

    Auto import all classes to __all__.

--- qtemplate/helpers/__init__.py
+++ qtemplate/helpers/__init__.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
-from .draggable import Draggable
-from .dropshadow import DropShadow
-from .repeater import Repeater
-from .stylesheet import StyleSheet
+import inspect
+import pkgutil
+from os.path import dirname, normpath
+from PySide6 import QtWidgets
+from ..base import QTemplateTag
 
-__all__ = [Draggable, DropShadow, Repeater, StyleSheet]
+# Load all QWidget and QTemplateTag classes into __all__ to make
+# importing these a bit more straight forward in qtemplate as well
+# as not require adding entries from newly created helpers.
+__all__ = []
+dirpath = normpath(f'{dirname(__file__)}')
+for loader, name, ispkg in pkgutil.iter_modules([dirpath]):
+    module = loader.find_module(name).load_module(name)
+    members = dict(inspect.getmembers(module, lambda obj: (inspect.isclass(obj)
+        and obj.__module__ == module.__name__
+        and issubclass(obj, (QtWidgets.QWidget, QTemplateTag))
+    )))
+    for clsname, cls in members.items():
+        __all__.append(cls)
